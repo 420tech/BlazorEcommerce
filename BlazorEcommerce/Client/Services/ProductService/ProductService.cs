8ae20e32a19f28552ad96dad26cb8f0448ae0ceb
Tim Belvin <timbelvin@hotmail.com>
2023-06-04 11:20:40 -0700

    Seedded more more products

--- BlazorEcommerce/Client/Services/ProductService/ProductService.cs
+++ BlazorEcommerce/Client/Services/ProductService/ProductService.cs
@@ -13,6 +13,12 @@ namespace BlazorEcommerce.Client.Services.ProductService
 
         public List<Product> Products { get; set; } = new();
 
+        public async Task<ServiceResponse<Product>> GetProduct(int productId)
+        {
+            var result = await _client.GetFromJsonAsync<ServiceResponse<Product>>($"api/product/{productId}");
+            return result;
+        }
+
         public async Task GetProducts()
         {
             var result = await _client.GetFromJsonAsync<ServiceResponse<List<Product>>>("api/product");
